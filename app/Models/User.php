8092520f64f98e762355ea2007539c006a0ead7d
Mohnd-Almsri <mohndmsre6789@gmail.com>
2024-11-29 15:37:03 +0300

    samir samkara

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
-
+//mohnd almsri is the best da goatttttttttttttttt
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
