b85c47ec8713b0a2086d8be48fbdce50c5d8888b
Mohnd-Almsri <mohndmsre6789@gmail.com>
2024-11-29 20:37:29 +0300

    m3az ajaj

--- app/Http/Controllers/VerificationCodeController.php
+++ app/Http/Controllers/VerificationCodeController.php
@@ -8,4 +8,24 @@ use Illuminate\Http\Request;
 
 class VerificationCodeController extends Controller
 {
+    public function verification(Request $request){
+
+        $request->validate([
+            'user_id' => 'required',
+            'code' => 'required'
+
+        ]);
+       $user_code = VerificationCode::where('user_id','=',$request->user_id)->pluck('code')->first();
+        if ($user_code==$request->code){
+            User::where('id','=',$request->user_id)->update(['number_verification'=>now()]);
+        return response()->json([
+            'status'=>'success',
+            'message'=>'Code Verified Successfully']);
+        }
+        else{return response()->json([
+            'status'=>'failed',
+            'message'=>'Code  did not verify']);
+        }
+
+    }
 }
