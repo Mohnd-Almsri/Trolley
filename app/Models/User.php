f742a8e8f19a4188c9727504e46e0969357833d3
M3aZ <52785801+M3aZFBI@users.noreply.github.com>
2024-11-29 16:20:14 +0300

    mohnd 7mar

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
+//ahmad mo7sn
 
 
 class User extends Authenticatable
