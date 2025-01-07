b93bca20eb3830b1ac0c65b4b92b8bb7ea5a5226
Mohnd-Almsri <mohndmsre6789@gmail.com>
2025-01-07 07:43:15 +0300

    اخر تعديل لمهند

--- routes/api.php
+++ routes/api.php
@@ -41,7 +41,7 @@ Route::post('/verification',  'verification');
 });
 Route::controller(StoreController::class)->middleware('auth:sanctum')->group(function () {
     Route::get('/StoreInfo', 'StoreInfo');
-    Route::get('/StoresForCategory', 'StoresForCategory');
+    Route::post('/StoresForCategory', 'StoresForCategory');
 });
 Route::controller(ProductController::class)->middleware('auth:sanctum')->group(function () {
     Route::get('/getProductInfo', 'getProductInfo');
